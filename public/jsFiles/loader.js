df9af3868509528dcb65f1ab3c2eccfd961cce74
defendet01 <r.r.rahman.r.r@gmail.com>
2020-06-26 23:03:28 +0600

    page preloader added

---
+++ public/jsFiles/loader.js
@@ -0,0 +1,7 @@
+//paste this code under head tag or in a seperate js file.
+// Wait for window load
+$(window).on("load", function() {
+  console.log("came here");
+  // Animate loader off screen
+  $(".se-pre-con").fadeOut("slow");
+});
