04015b54b83f6160a85e53ec0fecf470ef3adfa3
defendet01 <r.r.rahman.r.r@gmail.com>
2020-07-03 16:21:35 +0600

    UPDATE

--- routes/home.js
+++ routes/home.js
@@ -8,6 +8,22 @@ const {
   getNewsDetails
 }= require('../controllers/userFunctions')
 
+const classes = [
+  "বাংলাদেশ",
+  "আন্তর্জাতিক",
+  "অর্থনীতি",     
+  "সাহিত্য",
+  "ক্যাম্পাস",
+  "শিক্ষা",    
+  "খেলা",
+  "বিজ্ঞান ও প্রযুক্তি",
+  "বিনোদন",
+  "উদ্ভাবন",
+  "মতামত",
+  "কর্মসূচী",
+];
+
+
 /* GET home page. */
 router.get("/", getHome);
 
@@ -16,6 +32,19 @@ router.get("/videos", getVideos);
 router.get("/news/:class", getSectionNews);
 
 router.get("/newsDetails/:id", getNewsDetails);
+
+router.get('/aboutUs', (req, res)=>{
+  res.render('aboutUs', {classes})
+})
+router.get('/termsAndConditions', (req, res)=>{
+  res.render('termsAndConditions', {classes})
+})
+router.get('/privacyPolicy', (req, res)=>{
+  res.render('privacyPolicy', {classes})
+})
+router.get('/contactUs', (req, res)=>{
+  res.render('contactUs', {classes})
+})
 
 router.get("/test", (req, res) => {
   res.render("test", {classes});
