4912eb2554a73bd9d3c38b650a1356982a67f333
defendet01 <r.r.rahman.r.r@gmail.com>
2020-07-27 22:51:51 +0600

    staff added

--- routes/home.js
+++ routes/home.js
@@ -51,9 +51,9 @@ router.get('/ad', (req, res)=>{
 
 router.get('/members', (req, res) => {
   let staffs = {
-    names: [ 'ফৌজিয়া আহমেদ', 'সাজ্জাদ হোসাইন', 'পলাশ দেব রায়','রায়হান শেইখ', 'মোঃ আবু তালহা', 'মোঃ সাগর ফকির', 'ইমামুল ইসলাম', 'পবিত্র পাল', 'আফরুনা সুপ্তি', 'তানভীর কায়সার', 'রোমানা আক্তার শান্তা'],
-    imgFiles: ['Fouzia_Ahmed.jpg', 'Sazzad_Hossain.jpeg', 'Palash_Deb_Ray.jpg', 'Rayhan_Sheik.jpg', 'Md_Abu_Talha.jpg', 'Md_Sagar_Fokir.jpg', 'Imamul_Islam.jpg', 'Pabitra_pal.jpg', 'Afruna_Supti.jpg', 'Tanveer_Kaiser.jpeg', 'Romana_Aktar_Shanta.jpg'],
-    roles: ['বার্তা সম্পাদক','উপ সম্পাদক', 'সাংবাদিক','সাংবাদিক',  'সাংবাদিক', 'সাংবাদিক', 'সাংবাদিক', 'সাংবাদিক', 'উপস্থাপক', 'উপস্থাপক', 'উপস্থাপক' ]
+    names: [ 'ফৌজিয়া আহমেদ', 'সাজ্জাদ হোসাইন', 'পলাশ দেব রায়','রায়হান শেইখ', 'মোঃ আবু তালহা', 'মোঃ সাগর ফকির', 'ইমামুল ইসলাম', 'পবিত্র পাল', 'আফরুনা সুপ্তি', 'তানভীর কায়সার', 'রোমানা আক্তার শান্তা', 'জাহান লুবনা'],
+    imgFiles: ['Fouzia_Ahmed.jpg', 'Sazzad_Hossain.jpeg', 'Palash_Deb_Ray.jpg', 'Rayhan_Sheik.jpg', 'Md_Abu_Talha.jpg', 'Md_Sagar_Fokir.jpg', 'Imamul_Islam.jpg', 'Pabitra_pal.jpg', 'Afruna_Supti.jpg', 'Tanveer_Kaiser.jpeg', 'Romana_Aktar_Shanta.jpg', 'Jahan_Lubna.jpeg'],
+    roles: ['বার্তা সম্পাদক','উপ সম্পাদক', 'সাংবাদিক','সাংবাদিক',  'সাংবাদিক', 'সাংবাদিক', 'সাংবাদিক', 'সাংবাদিক', 'উপস্থাপক', 'উপস্থাপক', 'উপস্থাপক', 'গ্রাফিক্স ডিজাইনার' ]
   }
   res.render('staff', {classes,staffs})
 })
